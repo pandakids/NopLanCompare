a98f0db74d94980de2e7b80e5926d0699f9e85e0
benny <benny1026@live.cn>
2019-10-30 10:40:24 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Xml;
+
+namespace NopLanCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compare Begining....");
+
+            XmlDocument xmlSourcedoc = new XmlDocument();
+            xmlSourcedoc.Load("language_pack_source.xml");
+
+            XmlNodeList allResources = xmlSourcedoc.GetElementsByTagName("LocaleResource");
+            XmlNode lanNodeInSource = xmlSourcedoc.GetElementsByTagName("Language")[0];
+
+
+            XmlDocument xmlTargetdoc = new XmlDocument();
+            xmlTargetdoc.Load("language_pack_target.xml");
+
+            XmlNodeList allTargetResources = xmlTargetdoc.GetElementsByTagName("LocaleResource");
+
+            XmlNode lanNodeInTarget = xmlTargetdoc.GetElementsByTagName("Language")[0];
+
+
+            XmlDocument newDoc = new XmlDocument();
+            //给xml创建节点
+            XmlElement rootNode = newDoc.CreateElement("Language");
+            newDoc.AppendChild(rootNode);
+
+            XmlNodeList newNodeList = newDoc.ChildNodes;
+
+            foreach (XmlElement node in allResources) {
+                var cureentNodeName = node.Attributes[0].Value;
+
+                //print all the LocaleResource which not in the source doc
+                XmlNode nodeInTarget = xmlTargetdoc.SelectSingleNode("//LocaleResource[@Name='"+ cureentNodeName+"']");
+                if (nodeInTarget == null) {
+                    Console.WriteLine(cureentNodeName);
+
+                    var newElement = newDoc.CreateElement("LocaleResource");
+                    newElement.SetAttribute("Name", cureentNodeName);
+
+                    var innerElement = newDoc.CreateElement("Value");
+                    innerElement.InnerText = node.InnerText;
+
+                    newElement.AppendChild(innerElement);
+
+                    rootNode.AppendChild(newElement);
+                }
+            }
+
+            newDoc.Save("newTarget.xml");
+        }
+    }
+}
